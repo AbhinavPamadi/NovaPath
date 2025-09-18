1b8b33ab78d1bbb65630f479b47a120f292c667f
AbhinavPamadi <abhinavpamadi@gmail.com>
2025-09-18 13:06:36 +0530

    Add firebaseConfig for novapath-fb8de

---
+++ firebaseConfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBticzKgrrym4mIQ9ZyXJPq19xF55QUwpo",
+  authDomain: "novapath-fb8de.firebaseapp.com",
+  projectId: "novapath-fb8de",
+  storageBucket: "novapath-fb8de.firebasestorage.app",
+  messagingSenderId: "1022483455072",
+  appId: "1:1022483455072:web:150d982f87b6dc5480b705",
+  measurementId: "G-QT017L06NZ",
+};
+
+export const app = initializeApp(firebaseConfig);
